0e4207609b657667eb392ff9510349a3ad99093a
saurabh prakash <saupraka@gmail.com>
2018-09-19 09:58:26 +0100

    DPR-779 Passenger public app-"View Your Visa" page validations

--- public/src/main/resources/static/js/authentication/login.js
+++ public/src/main/resources/static/js/authentication/login.js
@@ -4,58 +4,60 @@ $(() => {
 
         event.preventDefault();
 
-        $('ul.error-summary-list').empty();
+        $('#error-list').empty();
         let passportError = false;
         let dobError = false;
 
         const day = $('#dob-day').val();
         const month = $('#dob-month').val();
         const year = $('#dob-year').val();
-
         if (!!!$('#passport-number').val().trim()) {
-            $('#passport-group').addClass('form-group-error');
-            $('#passport-number').toggleClass('invalid-input');
-            $('ul.error-summary-list').append('<li><a href="#passport-group">Please enter your passport number</a></li>');
+            $('#passport-group').addClass('govuk-form-group--error');
+            $('#passport-number').toggleClass('govuk-input--error');
+            $('#error-list').append('<li><a href="#passport-group">Please enter your passport number</a></li>');
             $('#passport-group .error-message').show();
             passportError = true;
         } else {
-            $('#passport-group').removeClass('form-group-error');
+            $('#passport-group').removeClass('govuk-form-group--error');
+            $('#passport-number').removeClass('govuk-input--error');
             $('#passport-group .error-message').hide();
         }
 
         if (!!!day || isNaN(day) || +day < 1 || +day > 31) {
-            $('#dob-day').addClass('invalid-input');
+            $('#dob-day').addClass('govuk-input--error');
             dobError = true;
         }
 
         if (!!!month || isNaN(month) || +month < 1 || +month > 12) {
-            $('#dob-month').addClass('invalid-input');
+            $('#dob-month').addClass('govuk-input--error');
             dobError = true;
         }
 
         if (!!!year || isNaN(year) || +year < 1900 || +year > (new Date()).getFullYear()) {
-            $('#dob-year').addClass('invalid-input');
+            $('#dob-year').addClass('govuk-input--error');
             dobError = true;
         }
 
         if (dobError) {
             $('#dob-group .error-message').show();
-            $('#dob-group').addClass('form-group-error');
+            $('#dob-group').addClass('govuk-form-group--error');
 
-            $('ul.error-summary-list').append('<li><a href="#dob-group">Please enter your date of birth, as written on your passport</a></li>');
+            $('ul.govuk-error-summary__list').append('<li><a href="#dob-group">Please enter your date of birth, as written on your passport</a></li>');
         } else {
             $('#dob-group .error-message').hide();
-            $('#dob-group').removeClass('form-group-error');
+            $('#dob-group').removeClass('govuk-form-group--error');
 
-            $('#dob-day').removeClass('invalid-input');
-            $('#dob-month').removeClass('invalid-input');
-            $('#dob-year').removeClass('invalid-input');
+            $('#dob-day').removeClass('govuk-input--error');
+            $('#dob-month').removeClass('govuk-input--error');
+            $('#dob-year').removeClass('govuk-input--error');
         }
 
         if (passportError || dobError) {
-            $('.validation-summary.error-summary').show();
+            $('#error-list-body').show();
+            $('.govuk-error-summary').removeAttr("style");
+            $('.govuk-error-summary').show();
         } else {
-            $('.validation-summary.error-summary').hide();
+            $('#error-list-body').hide();
 
             $.get('/csrf',function(data) {
                 $('input[name=' + data.parameterName + ']').val(data.token);
